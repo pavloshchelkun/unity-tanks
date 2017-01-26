24539b46e1bc7288652404268b129f63002e2f2e
Pavlo Shchelkun <pavel.shchelkun@gmail.com>
2017-01-26 17:27:06 +0200

    Firing Shells

--- Assets/Scripts/Tank/TankShooting.cs
+++ Assets/Scripts/Tank/TankShooting.cs
@@ -3,22 +3,21 @@ using UnityEngine.UI;
 
 public class TankShooting : MonoBehaviour
 {
-    public int m_PlayerNumber = 1;       
-    public Rigidbody m_Shell;            
-    public Transform m_FireTransform;    
-    public Slider m_AimSlider;           
-    public AudioSource m_ShootingAudio;  
-    public AudioClip m_ChargingClip;     
-    public AudioClip m_FireClip;         
-    public float m_MinLaunchForce = 15f; 
-    public float m_MaxLaunchForce = 30f; 
+    public int m_PlayerNumber = 1;
+    public Rigidbody m_Shell;
+    public Transform m_FireTransform;
+    public Slider m_AimSlider;
+    public AudioSource m_ShootingAudio;
+    public AudioClip m_ChargingClip;
+    public AudioClip m_FireClip;
+    public float m_MinLaunchForce = 15f;
+    public float m_MaxLaunchForce = 30f;
     public float m_MaxChargeTime = 0.75f;
 
-    /*
-    private string m_FireButton;         
-    private float m_CurrentLaunchForce;  
-    private float m_ChargeSpeed;         
-    private bool m_Fired;                
+    private string m_FireButton;
+    private float m_CurrentLaunchForce;
+    private float m_ChargeSpeed;
+    private bool m_Fired;
 
 
     private void OnEnable()
@@ -34,16 +33,68 @@ public class TankShooting : MonoBehaviour
 
         m_ChargeSpeed = (m_MaxLaunchForce - m_MinLaunchForce) / m_MaxChargeTime;
     }
-    */
 
     private void Update()
     {
         // Track the current state of the fire button and make decisions based on the current launch force.
+
+        // The slider should have a default value of the minimum launch force.
+        m_AimSlider.value = m_MinLaunchForce;
+
+        // If the max force has been exceeded and the shell hasn't yet been launched...
+        if (m_CurrentLaunchForce >= m_MaxLaunchForce && !m_Fired)
+        {
+            // ... use the max force and launch the shell.
+            m_CurrentLaunchForce = m_MaxLaunchForce;
+            Fire();
+        }
+        // Otherwise, if the fire button has just started being pressed...
+        else if (Input.GetButtonDown(m_FireButton))
+        {
+            // ... reset the fired flag and reset the launch force.
+            m_Fired = false;
+            m_CurrentLaunchForce = m_MinLaunchForce;
+
+            // Change the clip to the charging clip and start it playing.
+            m_ShootingAudio.clip = m_ChargingClip;
+            m_ShootingAudio.Play();
+        }
+        // Otherwise, if the fire button is being held and the shell hasn't been launched yet...
+        else if (Input.GetButton(m_FireButton) && !m_Fired)
+        {
+            // Increment the launch force and update the slider.
+            m_CurrentLaunchForce += m_ChargeSpeed * Time.deltaTime;
+
+            m_AimSlider.value = m_CurrentLaunchForce;
+        }
+        // Otherwise, if the fire button is released and the shell hasn't been launched yet...
+        else if (Input.GetButtonUp(m_FireButton) && !m_Fired)
+        {
+            // ... launch the shell.
+            Fire();
+        }
     }
 
 
     private void Fire()
     {
         // Instantiate and launch the shell.
+
+        // Set the fired flag so only Fire is only called once.
+        m_Fired = true;
+
+        // Create an instance of the shell and store a reference to it's rigidbody.
+        Rigidbody shellInstance =
+            Instantiate(m_Shell, m_FireTransform.position, m_FireTransform.rotation) as Rigidbody;
+
+        // Set the shell's velocity to the launch force in the fire position's forward direction.
+        shellInstance.velocity = m_CurrentLaunchForce * m_FireTransform.forward;
+
+        // Change the clip to the firing clip and play it.
+        m_ShootingAudio.clip = m_FireClip;
+        m_ShootingAudio.Play();
+
+        // Reset the launch force.  This is a precaution in case of missing button events.
+        m_CurrentLaunchForce = m_MinLaunchForce;
     }
 }
