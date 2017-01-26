3c99f1ddde2fb5f68566d698b0753e1cd5ec83a4
Pavlo Shchelkun <pavel.shchelkun@gmail.com>
2017-01-26 15:00:22 +0200

    Camera Control

--- Assets/Scripts/Camera/CameraControl.cs
+++ Assets/Scripts/Camera/CameraControl.cs
@@ -2,16 +2,17 @@
 
 public class CameraControl : MonoBehaviour
 {
-    public float m_DampTime = 0.2f;                 
-    public float m_ScreenEdgeBuffer = 4f;           
-    public float m_MinSize = 6.5f;                  
-    [HideInInspector] public Transform[] m_Targets; 
+    public float m_DampTime = 0.2f;
+    public float m_ScreenEdgeBuffer = 4f;
+    public float m_MinSize = 6.5f;
+    //[HideInInspector]
+    public Transform[] m_Targets;
 
 
-    private Camera m_Camera;                        
-    private float m_ZoomSpeed;                      
-    private Vector3 m_MoveVelocity;                 
-    private Vector3 m_DesiredPosition;              
+    private Camera m_Camera;
+    private float m_ZoomSpeed;
+    private Vector3 m_MoveVelocity;
+    private Vector3 m_DesiredPosition;
 
 
     private void Awake()
@@ -80,11 +81,11 @@ public class CameraControl : MonoBehaviour
 
             Vector3 desiredPosToTarget = targetLocalPos - desiredLocalPos;
 
-            size = Mathf.Max (size, Mathf.Abs (desiredPosToTarget.y));
+            size = Mathf.Max(size, Mathf.Abs(desiredPosToTarget.y));
 
-            size = Mathf.Max (size, Mathf.Abs (desiredPosToTarget.x) / m_Camera.aspect);
+            size = Mathf.Max(size, Mathf.Abs(desiredPosToTarget.x) / m_Camera.aspect);
         }
-        
+
         size += m_ScreenEdgeBuffer;
 
         size = Mathf.Max(size, m_MinSize);
